8f26ff327edbb127d741376aba41ccdb892524aa
Alex Crawford <awc737@gmail.com>
2014-12-09 11:18:33 -0800

    no slider arrow

--- js/app.js
+++ js/app.js
@@ -2,11 +2,12 @@ $(document).foundation();
 
 $(document).ready(function(){
 	$('#recent_projects_slider').slick({
-		arrows: true,
+		arrows: false,
 		dots: true,
 		slidesToShow: 3
 	});
 	$('#who_i_am_slider').slick({
+		arrows: false,
 		autoplay: true,
 		autoplaySpeed: 1700,
 		fade: false
