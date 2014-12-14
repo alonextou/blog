96843000f91c1297bf8243387ad96e9cc6b01efd
Alex Crawford <awc737@gmail.com>
2014-12-14 12:16:22 -0800

    helluva refactor

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,29 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    copy: {
+      main: {
+        cwd: 'app/',
+        src: 'js/**/*',
+        dest: 'public/assets/',
+        expand: true
+      },
+    },
+
     sass: {
       options: {
-        includePaths: ['bower_components/foundation/scss']
+        includePaths: [
+          'bower_components/foundation/scss',
+          'bower_components/font-awesome/scss',
+          'bower_components/slick.js/slick'
+        ],
       },
       dist: {
         options: {
           outputStyle: 'compressed'
         },
         files: {
-          'css/app.css': 'scss/app.scss'
+          'public/assets/css/app.css': 'app/scss/app.scss'
         }        
       }
     },
@@ -20,7 +33,7 @@ module.exports = function(grunt) {
       grunt: { files: ['Gruntfile.js'] },
 
       sass: {
-        files: 'scss/**/*.scss',
+        files: 'app/scss/**/*.scss',
         tasks: ['sass']
       }
     }
@@ -28,6 +41,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.registerTask('build', ['sass']);
   grunt.registerTask('default', ['build','watch']);
--- todo.md
+++
@@ -1,1 +0,0 @@
-http://tympanus.net/codrops/2013/06/18/caption-hover-effects/
